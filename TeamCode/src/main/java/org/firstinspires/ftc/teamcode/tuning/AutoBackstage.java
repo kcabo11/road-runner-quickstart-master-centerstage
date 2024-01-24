d2e9109bdba9aff7712ccd688c97bcce556716f6
kcabo11 <71565123+kcabo11@users.noreply.github.com>
2024-01-23 23:20:21 -0700

    Restructured Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/AutoBackstage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/AutoBackstage.java
@@ -1,10 +1,26 @@
 package org.firstinspires.ftc.teamcode.tuning;
 
+import static org.firstinspires.ftc.teamcode.tuning.AutoBackstage.START_POSITION.BLUE_BACKSTAGE;
+import static org.firstinspires.ftc.teamcode.tuning.AutoBackstage.START_POSITION.BLUE_STAGE;
+import static org.firstinspires.ftc.teamcode.tuning.AutoBackstage.START_POSITION.RED_BACKSTAGE;
+import static org.firstinspires.ftc.teamcode.tuning.AutoBackstage.START_POSITION.RED_STAGE;
+
+import org.firstinspires.ftc.teamcode.utils.ColorDetectionProcessor;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.TankDrive;
@@ -12,100 +28,200 @@ import org.firstinspires.ftc.teamcode.tuning.TuningOpModes;
 
 @Autonomous
 public final class AutoBackstage extends LinearOpMode {
+    enum START_POSITION {
+        //IN RELATION TO BACKBOARD
+        BLUE_BACKSTAGE,
+        BLUE_STAGE,
+        RED_BACKSTAGE,
+        RED_STAGE,
+        // ????
+        UNKNOWN
+    }
+    private static START_POSITION startPosition = START_POSITION.UNKNOWN; //WHERE WE ARE ON THE FIELD/ RED CLOSE ETC
+
+    private ColorDetectionProcessor colorDetectionProcessor;
+    private ColorDetectionProcessor.StartingPosition purplePixelPath = ColorDetectionProcessor.StartingPosition.CENTER;
+
+    public void initLoop() {
+        detectPurplePath();
+        telemetry.addData("Selected Auto: ", startPosition.toString());
+        telemetry.update();
+    }
+    private void detectPurplePath() {
+        purplePixelPath = colorDetectionProcessor.getPosition();
+        telemetry.addData("Purple Pixel Path: ", purplePixelPath.toString());
+    }
+
     @Override
     public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(14.5, 53.5, Math.toRadians(270));
+
         if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
+            Pose2d beginPose = new Pose2d(14.5, 53.5, Math.toRadians(270));
             MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
 
+            selectStartingPosition(); //FIND FIELD POSITION & COLOR
+
+
+            telemetry.addData("Camera preview on/off", "3 dots, Camera Stream");
+            telemetry.addData(">", "Touch Play to start OpMode");
+            telemetry.update();
+
             waitForStart();
 
-            Actions.runBlocking(
-                drive.actionBuilder(beginPose)
-                        // Center Pixel Placement
-//                        .splineTo(new Vector2d(18, 25.5), Math.toRadians(270))
-//                        // strafe left 29 inches
-//                        .strafeTo(new Vector2d(47,25.5))
+            //OVERRIDE FOR TESTING
 
-                        // Left Pixel Placement
-                        // .lineToY(47)
-                        .splineTo(new Vector2d(23, 38), Math.toRadians(270))
-//                        .waitSeconds(1)
-                        .setTangent(0)
-                        .splineToLinearHeading(new Pose2d(23, 43, Math.toRadians(270)), Math.toRadians(270))
-//                        // strafe left 29 inches
-                        .strafeTo(new Vector2d(47,15))
+
+            if (startPosition == BLUE_BACKSTAGE) {
+                switch (purplePixelPath) {
+                    case LEFT:
+                        beginPose = new Pose2d(14.5, 53.5, Math.toRadians(270));
+                        Actions.runBlocking(
+                                drive.actionBuilder(beginPose)
+                                .splineTo(new Vector2d(23, 38), Math.toRadians(270))
+                                .setTangent(0)
+                                .splineToLinearHeading(new Pose2d(23, 43, Math.toRadians(270)), Math.toRadians(270))
+                                .strafeTo(new Vector2d(47,15))
+                                .build());
+                    case CENTER:
+                        break;
+                    case RIGHT:
+                        break;
+                }
+            } else if (startPosition == BLUE_STAGE) {
+                switch (purplePixelPath) {
+                    case LEFT:
+                        break;
+                    case CENTER:
+                        break;
+                    case RIGHT:
+                        break;
+                }
+            } else if (startPosition == RED_BACKSTAGE) {
+                switch (purplePixelPath) {
+                    case LEFT:
+                        break;
+                    case CENTER:
+                        break;
+                    case RIGHT:
+                        break;
+                }
+            } else if (startPosition == RED_STAGE) {
+                switch (purplePixelPath) {
+                    case LEFT:
+                        break;
+                    case CENTER:
+                        break;
+                    case RIGHT:
+                        break;
+                }
+            }
+
+
+//                Actions.runBlocking(
+//                    drive.actionBuilder(beginPose)
+//                            // Center Pixel Placement
+//    //                        .splineTo(new Vector2d(18, 25.5), Math.toRadians(270))
+//    //                        // strafe left 29 inches
+//    //                        .strafeTo(new Vector2d(47,25.5))
 //
-//                        // Right Pixel Placement
-//                        .splineTo(new Vector2d(1, 38), Math.toRadians(270))
-//                        // strafe left 29 inches
-//                        .strafeTo(new Vector2d(47,25.5))
+//                            // Left Pixel Placement
+//                            // .lineToY(47)
+//                            .splineTo(new Vector2d(23, 38), Math.toRadians(270))
+//    //                        .waitSeconds(1)
+//                            .setTangent(0)
+//                            .splineToLinearHeading(new Pose2d(23, 43, Math.toRadians(270)), Math.toRadians(270))
+//    //                        // strafe left 29 inches
+//                            .strafeTo(new Vector2d(47,15))
+    //
+    //                        // Right Pixel Placement
+    //                        .splineTo(new Vector2d(1, 38), Math.toRadians(270))
+    //                        // strafe left 29 inches
+    //                        .strafeTo(new Vector2d(47,25.5))
 
 
-                        /* Placing pixel code here
-                        .waitSeconds(2)
-                        .setTangent(0)
-                        .splineTo(new Vector2d(36, 34.5), Math.toRadians(180))
-                        */
+                            /* Placing pixel code here
+                            .waitSeconds(2)
+                            .setTangent(0)
+                            .splineTo(new Vector2d(36, 34.5), Math.toRadians(180))
+                            */
 
 
-                        // CENTER POS
-                       // .lineToY(-27)
-                        //.back(2)
+                            // CENTER POS
+                           // .lineToY(-27)
+                            //.back(2)
 
-                        // sleep
-                        //.splineToSplineHeading(new Pose2d(45, -35, Math.toRadians(180)), Math.toRadians(0))
+                            // sleep
+                            //.splineToSplineHeading(new Pose2d(45, -35, Math.toRadians(180)), Math.toRadians(0))
 
-                        // LEFT POS
-                        // .strafeRight(10)
-                        // .forward(5)
-                       //  .splineToSplineHeading(new Pose2d(25, -35, Math.toRadians(180)), Math.toRadians(0))
-                        // .forward(13)
+                            // LEFT POS
+                            // .strafeRight(10)
+                            // .forward(5)
+                           //  .splineToSplineHeading(new Pose2d(25, -35, Math.toRadians(180)), Math.toRadians(0))
+                            // .forward(13)
 
-                        // sleep
-                        // .back(34)
+                            // sleep
+                            // .back(34)
 
-                        // RIGHT POS
-                        // .forward(30)
-                        // .strafeLeft(2)
-                        //.turn(Math.toRadians(-90))
-                        // .forward(3)
-                        // .back(2)
-                        // .strafeRight(23)
-                        // .forward(20)
-                        // .splineToSplineHeading(new Pose2d(48, -35, Math.toRadians(180)), Math.toRadians(0))
+                            // RIGHT POS
+                            // .forward(30)
+                            // .strafeLeft(2)
+                            //.turn(Math.toRadians(-90))
+                            // .forward(3)
+                            // .back(2)
+                            // .strafeRight(23)
+                            // .forward(20)
+                            // .splineToSplineHeading(new Pose2d(48, -35, Math.toRadians(180)), Math.toRadians(0))
 
-                        // .strafeLeft(22)
+                            // .strafeLeft(22)
 
-                        // .forward(47)
-                        // .turn(Math.toRadians(90))
-                        // .forward(3)
-                        // .back(5)
-                        // .turn(Math.toRadians(180))
-                        // .forward(115)
-                        // .forward(
+                            // .forward(47)
+                            // .turn(Math.toRadians(90))
+                            // .forward(3)
+                            // .back(5)
+                            // .turn(Math.toRadians(180))
+                            // .forward(115)
+                            // .forward(
 
-
-                        // MeepMeep Code here
-
-
-
-
-
-
-                        .build());
-        } else if (TuningOpModes.DRIVE_CLASS.equals(TankDrive.class)) {
-            TankDrive drive = new TankDrive(hardwareMap, beginPose);
-
-            waitForStart();
-
-            Actions.runBlocking(
-                    drive.actionBuilder(beginPose)
-                            .splineTo(new Vector2d(30, 30), Math.PI / 2)
-                            .splineTo(new Vector2d(0, 60), Math.PI)
-                            .build());
+//
+//                        .build());
         } else {
             throw new RuntimeException();
         }
     }
+
+    public void selectStartingPosition() {
+        //******select start pose*****
+        while (!isStopRequested()) {
+            telemetry.addLine("Glendale Qualifier - Code Initialized");
+            telemetry.addData("---------------------------------------", "");
+            telemetry.addLine("Select Starting Position using DPAD Keys");
+            telemetry.addData("    Blue Left   ", "(^)");
+            telemetry.addData("    Blue Right ", "(v)");
+            telemetry.addData("    Red Left    ", "(<)");
+            telemetry.addData("    Red Right  ", "(>)");
+
+            if (gamepad1.dpad_up || gamepad2.dpad_up) {
+                startPosition = BLUE_BACKSTAGE;
+                break;
+            }
+            if (gamepad1.dpad_down || gamepad2.dpad_down) {
+                startPosition = BLUE_STAGE;
+                break;
+            }
+            if (gamepad1.dpad_left || gamepad2.dpad_left) {
+                startPosition = START_POSITION.RED_BACKSTAGE;
+                break;
+            }
+            if (gamepad1.dpad_right || gamepad2.dpad_right) {
+                startPosition = START_POSITION.RED_STAGE;
+                break;
+            }
+            telemetry.update();
+        }
+        telemetry.clear();
+    }
+
+
+
+
 }
