b88c97f5f0e0b79f6e467543682bd3f8d99cb7b9
kcabo11 <71565123+kcabo11@users.noreply.github.com>
2024-02-17 04:58:38 -0700

    Changed TURN_SPEED to .3 to prevent dead stops during auto when too much draw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Autonomous_ParkRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Autonomous_ParkRight.java
@@ -132,7 +132,7 @@ public class Autonomous_ParkRight extends LinearOpMode {
     // These constants define the desired driving/control characteristics
     // They can/should be tweaked to suit the specific robot drive train.
     static final double     DRIVE_SPEED             = 0.3;     // Max driving speed for better distance accuracy.
-    static double     TURN_SPEED              = 0.2;     // Max Turn speed to limit turn rate
+    static double     TURN_SPEED              = 0.3;     // Max Turn speed to limit turn rate
     static final double     HEADING_THRESHOLD       = 1.0 ;    // How close must the heading get to the target before moving to next step.
     // Requiring more accuracy (a smaller number) will often make the turn take longer to get into the final position.
     // Define the Proportional control coefficient (or GAIN) for "heading control".
